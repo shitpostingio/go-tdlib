0dcacc625ce5aa82f5eaba34034b0d43f2d47962
Alessandro Pomponio <10339005+AlessandroPomponio@users.noreply.github.com>
2021-12-29 20:38:50 +0100

    Create autoupdate.sh

---
+++ autoupdate.sh
@@ -0,0 +1,27 @@
+#
+latest_repo_update=$(git log -1 --pretty="format:%ct")
+
+#
+git clone https://github.com/tdlib/td.git
+latest_tdschema_update=$(cd td && git log -1 --pretty="format:%ct" td/generate/scheme)
+latest_commit=$(cd td && git log -1 --pretty="format:%h")
+rm -rf td
+
+#
+if [ $latest_tdschema_update -gt $latest_repo_update ]; then
+
+        #
+        echo "Updating the generated code to tdlib commit $latest_commit"
+
+        # Remove the first line of the makefile
+        sed -i 1d Makefile
+
+        # Re-add the tag with the correct commit ID
+        sed -i "1iTAG := $latest_commit" Makefile
+
+        #
+        make schema-update
+        make generate-json
+        make generate-code
+
+fi
