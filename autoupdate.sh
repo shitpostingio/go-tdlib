1934868d0e3c822c42b5284f1306120f15575d1e
Alessandro Pomponio <10339005+AlessandroPomponio@users.noreply.github.com>
2021-12-30 08:56:42 +0000

    Use GitHub's REST API to fetch data about the repos

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,29 +1,26 @@
-#
-latest_makefile_update=$(git log -1 --pretty="format:%ct" Makefile)
+# Fetch latest update for our repo in the autoupdate branch
+latest_repo_update=$(curl "https://api.github.com/repos/shitpostingio/go-tdlib/commits?sha=autoupdate&path=Makefile&page=1&per_page=1" | jq -r ".[0].commit.author.date")
+lru_date=$(date --date=$latest_repo_update +%s)
+
+# 
+latest_tdlib_update=$(curl "https://api.github.com/repos/tdlib/td/commits?path=td/generate/scheme/td_api.tl&page=1&per_page=1" | jq -r ".[0].commit.author.date")
+ltu_date=$(date --date=$latest_tdlib_update +%s)
 
 #
-git clone https://github.com/tdlib/td.git
-latest_tdschema_update=$(cd td && git log -1 --pretty="format:%ct" td/generate/scheme/td_api.tl)
-latest_commit=$(cd td && git log -1 --pretty="format:%h")
+latest_tdlib_commit=$(curl "https://api.github.com/repos/tdlib/td/commits?&page=1&per_page=1" | jq -r ".[0].sha")
+latest_tdlib_commit=${latest_tdlib_commit:0:7}
 
 #
-echo "Latest Makefile update: $(git log -1 --pretty="format:%ci" Makefile)"
-echo "Latest td_api.tl update: $(cd td && git log -1 --pretty="format:%ci" td/generate/scheme/td_api.tl)"
-
-#
-rm -rf td
-
-#
-if [ $latest_tdschema_update -gt $latest_makefile_update ]; then
+if [ $ltu_date -gt $lru_date ]; then
 
         #
-        echo "Updating the generated code to tdlib commit $latest_commit"
+        echo "Updating the generated code to tdlib commit $latest_tdlib_commit"
 
         # Remove the first line of the makefile
         sed -i 1d Makefile
 
         # Re-add the tag with the correct commit ID
-        sed -i "1iTAG := $latest_commit" Makefile
+        sed -i "1iTAG := $latest_tdlib_commit" Makefile
 
         #
         make schema-update
